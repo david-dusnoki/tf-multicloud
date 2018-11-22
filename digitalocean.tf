a664c439910f71ab760a1c602e13065a47478a6d
David Dusnoki <me@daviddusnoki.tech>
2018-11-23 00:53:11 +0700

    Adding login info (commented) to digitalocean.tf

--- digitalocean.tf
+++ digitalocean.tf
@@ -3,6 +3,8 @@ resource "digitalocean_ssh_key" "do_ssh_key" {
     public_key = "${var.do_ssh_key}"
 }
 
+# You can ssh into the machine with root
+# The root password will be sent to your email address
 resource "digitalocean_droplet" "do-test-droplet" {
     image  = "ubuntu-16-04-x64"
     name   = "do-test-droplet"
