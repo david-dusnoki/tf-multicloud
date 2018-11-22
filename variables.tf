ee268abc6fe3d6779ff8074fa7263b28a9c7e369
David Dusnoki <me@daviddusnoki.tech>
2018-11-23 00:50:53 +0700

    Adding digital ocean token and SSH key as variables

--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
+# Azure
 variable azure_subscription_id {}
 variable azure_client_id {}
 variable azure_client_secret {}
 variable azure_tenant_id {}
 variable azure_ssh_key {}
 variable azure_username {}
+
+# Digital Ocean
+variable do_token {}
+variable do_ssh_key {}
