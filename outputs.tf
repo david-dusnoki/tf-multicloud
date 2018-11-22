d4c2333407a3a6fe60531ef01aba77f8aeb7e6d6
David Dusnoki <me@daviddusnoki.tech>
2018-11-23 00:51:26 +0700

    Adding Digital Ocean public IP as output

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,9 @@ output "azure_public_ip" {
 output "azure_username" {
     value = "${var.azure_username}"
 }
+
+# Digital Ocean
+
+output "do_public_ip" {
+    value = "${digitalocean_droplet.do-test-droplet.ipv4_address}"
+}
