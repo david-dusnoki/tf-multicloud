2730d409426b770d426ab69ad6962d8dc76d4e9d
David Dusnoki <me@daviddusnoki.tech>
2018-11-23 00:50:18 +0700

    Adding Digital Ocean as provider

--- providers.tf
+++ providers.tf
@@ -5,3 +5,9 @@ provider "azurerm" {
     client_secret   = "${var.azure_client_secret}"
     tenant_id       = "${var.azure_tenant_id}"
 }
+
+# Configure Digital Ocean Provider
+provider "digitalocean" {
+    token = "${var.do_token}"
+}
+
