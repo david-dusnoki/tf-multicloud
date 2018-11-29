4e877184013cfca65b39e964c9f4e3869d733817
David Dusnoki <me@daviddusnoki.tech>
2018-11-29 16:13:39 +0700

    Adding Terraform export to Ansible inventory

---
+++ ansible-export.tf
@@ -0,0 +1,20 @@
+data "template_file" "dev_hosts" {
+    template = "${file("${path.module}/hosts.cfg")}"
+    depends_on = [
+        "azurerm_public_ip.test-publicip",
+        "digitalocean_droplet.do-test-droplet"
+    ]
+    vars {
+        azure-ip = "${azurerm_public_ip.test-publicip.ip_address}"
+        do-ip = "${digitalocean_droplet.do-test-droplet.ipv4_address}"
+    }
+}
+
+resource "null_resource" "dev-hosts" {
+    triggers {
+        template_rendered = "${data.template_file.dev_hosts.rendered}"
+    }
+    provisioner "local-exec" {
+        command = "echo '${data.template_file.dev_hosts.rendered}' > dev_hosts"
+    }
+}
