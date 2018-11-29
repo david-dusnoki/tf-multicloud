9d10b6186ac592f47b07efce5ffb83eae4dd4cdd
David Dusnoki <me@daviddusnoki.tech>
2018-11-29 21:23:05 +0700

    Correcting source of Azure IP address

--- ansible-export.tf
+++ ansible-export.tf
@@ -1,20 +1,20 @@
-data "template_file" "dev_hosts" {
+data "template_file" "hosts" {
     template = "${file("${path.module}/hosts.cfg")}"
     depends_on = [
         "azurerm_public_ip.test-publicip",
         "digitalocean_droplet.do-test-droplet"
     ]
     vars {
-        azure-ip = "${azurerm_public_ip.test-publicip.ip_address}"
+        azure-ip = "${data.azurerm_public_ip.test-publicip.ip_address}"
         do-ip = "${digitalocean_droplet.do-test-droplet.ipv4_address}"
     }
 }
 
-resource "null_resource" "dev-hosts" {
+resource "null_resource" "hosts" {
     triggers {
-        template_rendered = "${data.template_file.dev_hosts.rendered}"
+        template_rendered = "${data.template_file.hosts.rendered}"
     }
     provisioner "local-exec" {
-        command = "echo '${data.template_file.dev_hosts.rendered}' > hosts"
+        command = "echo '${data.template_file.hosts.rendered}' > hosts"
     }
 }
