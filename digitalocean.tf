0bd78b54859489ffc757f0c9e2400a9921fef28e
David Dusnoki <me@daviddusnoki.tech>
2018-12-11 04:37:02 +0700

    Adding variable name

--- digitalocean.tf
+++ digitalocean.tf
@@ -7,7 +7,7 @@ resource "digitalocean_ssh_key" "do_ssh_key" {
 # The root password will be sent to your email address
 resource "digitalocean_droplet" "do-test-droplet" {
     image  = "ubuntu-16-04-x64"
-    name   = "do-test-droplet"
+    name   = "do-${var.name}-droplet"
     region = "sgp1"
     size   = "1gb"
     ssh_keys = ["${digitalocean_ssh_key.do_ssh_key.fingerprint}"]
