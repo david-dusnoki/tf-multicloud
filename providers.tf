2d3fe54d55ba3b11b0332a7f8f0db38c6b4057a4
David Dusnoki <me@daviddusnoki.tech>
2018-12-17 17:16:09 +0700

    Adding AWS provider

--- providers.tf
+++ providers.tf
@@ -17,3 +17,10 @@ provider "digitalocean" {
 #    project     = "durable-timing-100905"
 #    region      = "us-west1"
 #}
+
+# Configure AWS Provider
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "us-west-1"
+}
