b01820d0607b2d4c851242ebbe24025ce0fa521a
David Dusnoki <me@daviddusnoki.tech>
2018-12-19 02:03:59 +0700

    Adding aws instance ip to ansible hosts

--- ansible-export.tf
+++ ansible-export.tf
@@ -2,11 +2,13 @@ data "template_file" "hosts" {
     template = "${file("${path.module}/hosts.cfg")}"
     depends_on = [
         "azurerm_public_ip.test-publicip",
-        "digitalocean_droplet.do-test-droplet"
+        "digitalocean_droplet.do-test-droplet",
+        "aws_instance.ec2-test"
     ]
     vars {
-        azure-ip = "${data.azurerm_public_ip.test-publicip.ip_address}"
-        do-ip = "${digitalocean_droplet.do-test-droplet.ipv4_address}"
+        azure-ip	= "${data.azurerm_public_ip.test-publicip.ip_address}"
+        do-ip		= "${digitalocean_droplet.do-test-droplet.ipv4_address}"
+        aws-ip		= "${aws_instance.ec2-test.public_ip}"
     }
 }
 
